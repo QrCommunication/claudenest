a0f64e4a0cd629681c3485c8d8cd1ca7080e158e
Rony <rony@qrcommunication.fr>
2026-04-12 23:20:46 +0200

    fix(button): use relative path for @reference in Tailwind v4

--- packages/server/resources/js/components/common/Button.vue
+++ packages/server/resources/js/components/common/Button.vue
@@ -90,7 +90,7 @@ const sizeClass = computed(() => {
 </script>
 
 <style scoped>
-@reference "/resources/css/tailwind.css";
+@reference "../../../../css/tailwind.css";
 
 /* Variants */
 .btn-primary {
